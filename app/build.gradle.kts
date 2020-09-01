697f92e97d1c80a24742d914ac878576b6e05cc4
Boris <boristul97@gmail.com>
2020-09-02 01:02:33 +0300

    Initial commit. Create project, add detekt-config.yml.

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        applicationId = "com.boristul.notebook"
+        versionCode = 1
+        versionName = "0.0.0"
+
+        minSdkVersion(21)
+        targetSdkVersion(29)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    // region Kotlin
+    val kotlinVersion: String by project
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    // endregion
+
+    // region Local
+    // endregion
+
+    // region Navigation
+    val navigationVersion: String by project
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    // endregion
+
+    // region Core
+    // endregion
+
+    // region androidX
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    // endregion
+}
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("io.gitlab.arturbosch.detekt") version ("1.12.0")
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        val kotlinVersion: String by project
+
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+subprojects.forEach { module ->
+    module.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            @Suppress("SuspiciousCollectionReassignment")
+            freeCompilerArgs += listOf(
+                //"-XXLanguage:+InlineClasses",
+                "-Xnew-inference"
+            )
+        }
+    }
+}
+
+detekt {
+    config = files("detekt-config.yml")
+    input = files(*subprojects.map { "${it.name}/src" }.toTypedArray())
+    reports { xml { enabled = false } }
+    failFast = false
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.12.0")
+}
