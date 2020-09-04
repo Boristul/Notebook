6f268253f4653997bfbaed1134af6654f9e143f2
Boris <boristul97@gmail.com>
2020-09-04 18:37:29 +0300

    Fix navigation. Add layout resource to fragment

--- app/src/main/kotlin/com/boristul/notebook/ui/notes/NotesFragment.kt
+++ app/src/main/kotlin/com/boristul/notebook/ui/notes/NotesFragment.kt
@@ -1,7 +1,7 @@
 package com.boristul.notebook.ui.notes
 
 import androidx.fragment.app.Fragment
+import com.boristul.notebook.R
 
-class NotesFragment : Fragment() {
-
+class NotesFragment : Fragment(R.layout.fragment_notes) {
 }
