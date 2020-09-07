bf58464006593a4abaf68ab3f28e9e5f4ea97cc6
Boris <boristul97@gmail.com>
2020-09-07 19:59:20 +0300

    Add top menu on NotesFragment.kt

--- app/src/main/kotlin/com/boristul/notebook/ui/notes/NotesFragment.kt
+++ app/src/main/kotlin/com/boristul/notebook/ui/notes/NotesFragment.kt
@@ -1,16 +1,25 @@
 package com.boristul.notebook.ui.notes
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.boristul.notebook.R
 import com.boristul.notebook.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment(R.layout.fragment_notes) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel by viewModels<NotesFragmentViewModel>()
         val binding = FragmentNotesBinding.bind(view)
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            setHasOptionsMenu(true)
+        }
 
         binding.notesList.apply {
             adapter = NotesListAdapter().apply {
@@ -19,5 +28,15 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) =
+        inflater.inflate(R.menu.top_main, menu)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.nav_action_add -> {
+            true
+        }
+        else -> false
     }
 }
