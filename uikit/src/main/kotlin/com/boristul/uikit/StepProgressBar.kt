6a42b0f636e0b9ddd4589d605d6c7e2b5f919fd4
Boris Tyulkin <boristul97@gmail.com>
2020-09-25 14:49:36 +0300

    Add suppress annotation for TooManyFunctions warning

--- uikit/src/main/kotlin/com/boristul/uikit/StepProgressBar.kt
+++ uikit/src/main/kotlin/com/boristul/uikit/StepProgressBar.kt
@@ -14,6 +14,7 @@ import androidx.annotation.Dimension
 import androidx.core.content.withStyledAttributes
 import com.boristul.utils.getColorCompat
 
+@Suppress("TooManyFunctions")
 class StepProgressBar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
