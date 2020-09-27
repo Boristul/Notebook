65b24ba2bd051a4d5af9872c76072c81a981830d
Boris <boristul97@gmail.com>
2020-09-28 02:24:34 +0300

    Fix detekt

--- uikit/src/main/kotlin/com/boristul/uikit/ThemeSelector.kt
+++ uikit/src/main/kotlin/com/boristul/uikit/ThemeSelector.kt
@@ -5,22 +5,22 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 
 object ThemeSelector {
-        private const val THEME_PREF = "theme_type_pref"
-        private const val DEFAULT_STRING = "Default"
+    private const val THEME_PREF = "theme_type_pref"
+    private const val DEFAULT_STRING = "Default"
 
-        // TODO: this is because of list pref use only string-arrays
-        fun initTheme(context: Context) {
-                PreferenceManager.getDefaultSharedPreferences(context).getString(
-                        THEME_PREF,
-                        DEFAULT_STRING
-                )?.also {
-                        AppCompatDelegate.setDefaultNightMode(
-                                if (it == DEFAULT_STRING) AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM else it.toInt()
-                        )
-                }
+    // TODO: this is because of list pref use only string-arrays
+    fun initTheme(context: Context) {
+        PreferenceManager.getDefaultSharedPreferences(context).getString(
+            THEME_PREF,
+            DEFAULT_STRING
+        )?.also {
+            AppCompatDelegate.setDefaultNightMode(
+                if (it == DEFAULT_STRING) AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM else it.toInt()
+            )
         }
+    }
 
-        fun setTheme(themeMode: Int) {
-                AppCompatDelegate.setDefaultNightMode(themeMode)
-        }
+    fun setTheme(themeMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+    }
 }
