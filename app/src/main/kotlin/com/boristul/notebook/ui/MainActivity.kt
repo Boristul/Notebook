ca873d3f2035150e4cab4be1eae1c09454683b57
Boris <boristul97@gmail.com>
2020-09-29 00:13:59 +0300

    Refactor navigation

--- app/src/main/kotlin/com/boristul/notebook/ui/MainActivity.kt
+++ app/src/main/kotlin/com/boristul/notebook/ui/MainActivity.kt
@@ -2,7 +2,7 @@ package com.boristul.notebook.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.boristul.notebook.R
+import androidx.navigation.ui.NavigationUI
 import com.boristul.notebook.databinding.ActivityMainBinding
 import com.boristul.utils.findNavControllerInOnCreate
 
@@ -13,21 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navController = findNavControllerInOnCreate(binding.navHostFragment.id).apply {
-            addOnDestinationChangedListener { _, destination, _ -> title = destination.label }
+            //addOnDestinationChangedListener { _, destination, _ -> title = destination.label }
         }
-
-        binding.navigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_action_notes -> {
-                    navController.navigate(R.id.nav_action_notes)
-                    true
-                }
-                R.id.nav_action_settings -> {
-                    navController.navigate(R.id.nav_action_settings)
-                    true
-                }
-                else -> false
-            }
-        }
+        NavigationUI.setupWithNavController(binding.navigation, navController)
     }
 }
