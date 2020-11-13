33563e048e26a0d766072cfd41db2101ef550098
Boris <boristul97@gmail.com>
2020-11-13 13:00:12 +0300

    Add firebase event log for SplashActivity.kt. Update gradle plugin. Refactor google-services.json

--- app/src/main/kotlin/com/boristul/notebook/ui/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/boristul/notebook/ui/splash/SplashActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.boristul.notebook.ui.MainActivity
 import com.boristul.utils.startActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity<MainActivity>()
+        FirebaseAnalytics.getInstance(this).apply {
+            logEvent("splash_activity", null)
+        }
         finish()
     }
 }
