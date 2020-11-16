597eb358aa181b0cec23ed43c3d210f797212f5c
Boris <boristul97@gmail.com>
2020-11-16 15:17:45 +0300

    Add user_id for firebase analytics

--- app/src/main/kotlin/com/boristul/notebook/MainApplication.kt
+++ app/src/main/kotlin/com/boristul/notebook/MainApplication.kt
@@ -3,6 +3,7 @@ package com.boristul.notebook
 import android.app.Application
 import com.boristul.repository.repositoryModule
 import com.boristul.uikit.ThemeSelector
+import com.google.firebase.analytics.FirebaseAnalytics
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
@@ -14,5 +15,10 @@ class MainApplication : Application(), DIAware {
     override fun onCreate() {
         super.onCreate()
         ThemeSelector.initTheme(applicationContext)
+
+        // TODO: Temporary code for demo of firebase statistics
+        FirebaseAnalytics
+            .getInstance(this)
+            .setUserId("0x88a007ec4f1819f24c0988fc9c26496b99b436d10x88a007ec4f1819f24c0988fc9c26496b99b436d1")
     }
 }
