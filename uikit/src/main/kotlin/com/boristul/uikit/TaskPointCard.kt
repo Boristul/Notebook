789df2ac84d772e49caf2ba681ef39de01bd359b
Boris Tyulkin <boristul97@gmail.com>
2020-11-25 18:53:32 +0300

    Start working with TaskPointCard.kt

---
+++ uikit/src/main/kotlin/com/boristul/uikit/TaskPointCard.kt
@@ -0,0 +1,13 @@
+package com.boristul.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import com.daimajia.swipe.SwipeLayout
+
+class TaskPointCard @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : SwipeLayout(context, attrs, defStyleAttr) {
+
+}
