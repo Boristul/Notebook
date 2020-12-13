b99a5dc7ae4f780a1271330ced50d952e8ed799c
Boris Tyulkin <boristul97@gmail.com>
2020-12-13 20:49:59 +0300

    Remove temporary firebase userId

--- app/src/main/kotlin/com/boristul/notebook/MainApplication.kt
+++ app/src/main/kotlin/com/boristul/notebook/MainApplication.kt
@@ -3,8 +3,6 @@ package com.boristul.notebook
 import android.app.Application
 import com.boristul.repository.repositoryModule
 import com.boristul.uikit.ThemeSelector
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
@@ -16,9 +14,5 @@ class MainApplication : Application(), DIAware {
     override fun onCreate() {
         super.onCreate()
         ThemeSelector.initTheme(applicationContext)
-
-        // TODO: Temporary code for demo of firebase statistics
-        Firebase.analytics
-            .setUserId("0x88a007ec4f1819f24c0988fc9c26496b99b436d10x88a007ec4f1819f24c0988fc9c26496b99b436d1")
     }
 }
