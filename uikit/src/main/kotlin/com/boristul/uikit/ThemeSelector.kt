f64c54311b02505a7c4a41281f7c334549e4795e
Boris Tyulkin <boristul97@gmail.com>
2020-12-23 09:53:28 +0300

    Refactor theme selector

--- uikit/src/main/kotlin/com/boristul/uikit/ThemeSelector.kt
+++ uikit/src/main/kotlin/com/boristul/uikit/ThemeSelector.kt
@@ -1,21 +1,30 @@
 package com.boristul.uikit
 
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 
 object ThemeSelector {
     private const val THEME_PREF = "theme_type_pref"
-    private const val DEFAULT_STRING = "Default"
+    private const val DEFAULT_VALUE = "-1"
 
     // TODO: this is because of list pref use only string-arrays
     fun initTheme(context: Context) {
         PreferenceManager.getDefaultSharedPreferences(context).getString(
             THEME_PREF,
-            DEFAULT_STRING
+            DEFAULT_VALUE
         )?.also {
             AppCompatDelegate.setDefaultNightMode(
-                if (it == DEFAULT_STRING) AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM else it.toInt()
+                if (it == DEFAULT_VALUE) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    } else {
+                        AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                    }
+                } else {
+                    it.toInt()
+                }
             )
         }
     }
