e55f0f5740e90ce113e1d4843ba47cbdfec2520f
Boris Tyulkin <boristul97@gmail.com>
2022-04-29 12:43:14 +0300

    Update ViewBindingDelegate.kt

--- utils/src/main/kotlin/com/boristul/utils/viewbinding/ViewBindingDelegate.kt
+++ utils/src/main/kotlin/com/boristul/utils/viewbinding/ViewBindingDelegate.kt
@@ -1,54 +1,78 @@
 package com.boristul.utils.viewbinding
 
-import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
+import androidx.lifecycle.Lifecycle.State.INITIALIZED
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
-import java.lang.reflect.Method
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-inline fun <reified T : ViewBinding> Fragment.viewBinding(): ViewBindingDelegate<T> =
-    ViewBindingDelegate(this, T::class)
+/**
+ * Delegate returning ViewBinding bounded to [Fragment]'s lifecycle.
+ *
+ * Usage:
+ * ```
+ *  class ProfileFragment : Fragment(R.layout.fragment_profile) {
+ *
+ *      private val binding: FragmentProfileBinging by viewBinding()
+ *
+ *      override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+ *          super.onViewCreated(view, savedInstanceState)
+ *
+ *          with(binding) {
+ *              // use binding fields
+ *          }
+ *      }
+ *  }
+ * ```
+ * Accessing `binding` before `onViewCreated` or after `onViewDestroy` throws [IllegalStateException].
+ */
+inline fun <reified VB : ViewBinding> Fragment.viewBinding(): ReadOnlyProperty<Any?, VB> {
+    return ViewBindingDelegate(this, VB::class)
+}
 
 /** @see viewBinding */
-class ViewBindingDelegate<T : ViewBinding> @PublishedApi internal constructor(
+@PublishedApi
+internal class ViewBindingDelegate<VB : ViewBinding> constructor(
     private val fragment: Fragment,
-    private val viewBindingClass: KClass<T>
-) : ReadOnlyProperty<Any?, T> {
+    private val viewBindingClass: KClass<VB>,
+) : ReadOnlyProperty<Any?, VB> {
 
-    private var binding: T? = null
+    private var binding: VB? = null
 
-    init {
-        fragment.viewLifecycleOwnerLiveData.observe(fragment) { lifecycleOwner ->
-            lifecycleOwner.lifecycle.addObserver(
-                LifecycleEventObserver { _, event ->
-                    if (event == Lifecycle.Event.ON_DESTROY) binding = null
+    private val bindingCleaner: LifecycleObserver by lazy {
+        object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                if (event == ON_DESTROY) {
+                    binding = null
+                    source.lifecycle.removeObserver(this)
                 }
-            )
+            }
         }
     }
 
-    override fun getValue(thisRef: Any?, property: KProperty<*>): T = binding ?: obtainBinding()
+    override fun getValue(thisRef: Any?, property: KProperty<*>): VB = binding ?: obtainBinding()
 
-    private fun obtainBinding(): T {
+    private fun obtainBinding(): VB {
         val view = checkNotNull(fragment.view) {
             "ViewBinding is only valid between onCreateView and onDestroyView."
         }
         return viewBindingClass.bind(view)
-            .also { binding = it }
+            .also(::saveBindingIfNeed)
+    }
+
+    private fun saveBindingIfNeed(binding: VB) {
+        val lifecycle = fragment.viewLifecycleOwner.lifecycle
+        // Save binding if view is not destroyed
+        if (lifecycle.currentState.isAtLeast(INITIALIZED)) {
+            this.binding = binding
+            // Clean binding on view destroy
+            lifecycle.addObserver(bindingCleaner)
+        }
     }
 }
-
-fun <T : ViewBinding> KClass<T>.bind(rootView: View): T {
-    val inflateMethod = java.getBindMethod()
-    @Suppress("UNCHECKED_CAST")
-    return inflateMethod.invoke(null, rootView) as T
-}
-
-private val bindMethodsCache = mutableMapOf<Class<out ViewBinding>, Method>()
-
-private fun Class<out ViewBinding>.getBindMethod(): Method =
-    bindMethodsCache.getOrPut(this) { getDeclaredMethod("bind", View::class.java) }
