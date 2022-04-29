a135bd63a3eae2b390b84f80dcaef71baa753a27
Boris Tyulkin <boristul97@gmail.com>
2022-04-29 14:19:42 +0300

    Fix lint

--- app/src/main/kotlin/com/boristul/notebook/ui/settings/tags/TagsFragment.kt
+++ app/src/main/kotlin/com/boristul/notebook/ui/settings/tags/TagsFragment.kt
@@ -5,11 +5,9 @@ import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.boristul.notebook.R
 import com.boristul.notebook.databinding.FragmentTagsBinding
+import com.boristul.utils.collectOnStarted
 import com.boristul.utils.distinctUntilChanged
 import com.boristul.utils.hideKeyboard
 import com.boristul.utils.setViewCount
@@ -17,8 +15,6 @@ import com.boristul.utils.viewbinding.viewBinding
 import com.google.android.material.chip.Chip
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TagsFragment : Fragment(R.layout.fragment_tags) {
@@ -27,28 +23,24 @@ class TagsFragment : Fragment(R.layout.fragment_tags) {
         val viewModel by viewModels<TagsFragmentViewModel>()
         val binding by viewBinding<FragmentTagsBinding>()
 
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.tags.collect { tags ->
-                    binding.chips.setViewCount(
-                        tags.size,
-                        { layoutInflater.inflate(R.layout.item_tag_chip_edit, this, false) as Chip },
-                        {
-                            setOnCloseIconClickListener { _ ->
-                                MaterialAlertDialogBuilder(requireContext())
-                                    .setTitle(getString(R.string.tf_delete_title, tags[it].name))
-                                    .setMessage(R.string.tf_delete_message)
-                                    .setPositiveButton(R.string.tf_delete) { _, _ ->
-                                        viewModel.delete(tags[it].id)
-                                    }
-                                    .setNegativeButton(R.string.tf_cancel, null)
-                                    .show()
+        viewModel.tags.collectOnStarted(viewLifecycleOwner) { tags ->
+            binding.chips.setViewCount(
+                tags.size,
+                { layoutInflater.inflate(R.layout.item_tag_chip_edit, this, false) as Chip },
+                {
+                    setOnCloseIconClickListener { _ ->
+                        MaterialAlertDialogBuilder(requireContext())
+                            .setTitle(getString(R.string.tf_delete_title, tags[it].name))
+                            .setMessage(R.string.tf_delete_message)
+                            .setPositiveButton(R.string.tf_delete) { _, _ ->
+                                viewModel.delete(tags[it].id)
                             }
-                            text = tags[it].name
-                        }
-                    )
+                            .setNegativeButton(R.string.tf_cancel, null)
+                            .show()
+                    }
+                    text = tags[it].name
                 }
-            }
+            )
         }
 
         binding.add.apply {
